9bbabc01eb739c0fa9850d095f6811a65e0e68df
uuuth <xoghd88@gmail.com>
2019-07-13 23:25:47 +0900

    2. 리스트 출력 작업 - controller 작성

--- src/main/java/com/uth/login/controller/MemberController.java
+++ src/main/java/com/uth/login/controller/MemberController.java
@@ -1,5 +1,31 @@
 package com.uth.login.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.uth.login.model.MemberVO;
+import com.uth.login.service.MemberService;
+
+@Controller
+@RequestMapping("/member/*")
 public class MemberController {
 
+	@Autowired
+	private MemberService service;
+	
+	@GetMapping("/list")
+	public void memberList(Model model){
+		
+		List<MemberVO> memberlist = new ArrayList<>();
+		
+		memberlist = service.memberlist();
+		
+		model.addAttribute("memberlist", memberlist);
+		
+	}
+	
 }
