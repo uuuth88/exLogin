b5e2427b202f5b6744ff3af2e4ec7ce33bddd785
uuuth <xoghd88@gmail.com>
2019-07-13 23:36:08 +0900

    2. 리스트 출력 작업 - controller 테스트 완료.

---
+++ src/test/java/com/uth/login/controller/MemberControllerTest.java
@@ -0,0 +1,40 @@
+package com.uth.login.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml", 
+					   "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class MemberControllerTest {
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void test() {
+		try {
+			System.out.println(mockMvc.perform(MockMvcRequestBuilders.get("/member/list"))
+				   .andReturn().getModelAndView());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
