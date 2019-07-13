7083742d5d1579b437431a0d958c7e10578c50b8
uuuth <xoghd88@gmail.com>
2019-07-13 20:46:01 +0900

    2. 리스트 출력 작업 - DaoImpl 작성, root-context.xml 수정

--- src/main/java/com/uth/login/dao/MemberDaoImpl.java
+++ src/main/java/com/uth/login/dao/MemberDaoImpl.java
@@ -2,14 +2,19 @@ package com.uth.login.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.uth.login.model.MemberVO;
-
+@Repository
 public class MemberDaoImpl implements MemberDao {
-
+	@Autowired
+	private SqlSession ss;
+	
 	@Override
 	public List<MemberVO> memberlist() {
-		// TODO Auto-generated method stub
-		return null;
+		return ss.selectList("com.uth.mapper.memberMapper.memberlist");
 	}
 
 }
