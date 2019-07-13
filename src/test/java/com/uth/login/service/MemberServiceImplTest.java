afd22a7f03c55041ae0b5fd5eb41f09a80863057
uuuth <xoghd88@gmail.com>
2019-07-13 23:12:15 +0900

    2. 리스트 출력 작업 - service 테스트

---
+++ src/test/java/com/uth/login/service/MemberServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.uth.login.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class MemberServiceImplTest {
+	@Autowired
+	private MemberService service;
+	
+	@Test
+	public void test() {
+		service.memberlist();
+	}
+
+}
